35b1526f634bbcd2fed80df5b29f05b5d1831f1f
andy fang <andy@mingtak.com.tw>
2015-03-03 06:12:25 +0000

    backup 20150303

--- funlog/theme/parameters.py
+++ funlog/theme/parameters.py
@@ -9,6 +9,20 @@ from plone import api
 class GetThemeId(BrowserView):
 
     def __call__(self):
-        owner = self.context.getOwner()
-        themeId = safe_unicode(owner.getProperty('blogTheme', 'porto'))
-        return themeId 
+        ownerId = self.context.owner_info()["id"]
+        catalog = self.context.portal_catalog
+        profile = catalog({"Creator":ownerId, "Type":"Profile"})[0]
+        return profile.blogTheme
+
+
+class GetContentType(BrowserView):
+
+    def __call__(self):
+        return self.context.Type()
+
+class IsOwner(BrowserView):
+
+    def __call__(self):
+        ownerId = self.context.owner_info()["id"]
+        currentUser = api.user.get_current().id
+        return (ownerId == currentUser)
