3b9cefdbc6f0f653066db1f6021208ae3b8ed15b
andy fang <andy@mingtak.com.tw>
2015-03-23 14:40:19 +0000

    add creatika_blue

--- funlog/theme/parameters.py
+++ funlog/theme/parameters.py
@@ -2,17 +2,32 @@ from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFPlone.utils import safe_unicode
 
-
 from plone import api
+
+
+class IsSystemPage(BrowserView):
+
+    def __call__(self):
+        ownerId = self.context.owner_info()["id"]
+#        import pdb; pdb.set_trace()
+        if 'Manager' in api.user.get_roles(username=ownerId):
+            self.isSystemPage = True
+        else:
+            self.isSystemPage = False
+        return self.isSystemPage
 
 
 class GetThemeId(BrowserView):
 
     def __call__(self):
         ownerId = self.context.owner_info()["id"]
+        if 'Manager' in api.user.get_roles(username=ownerId):
+            return u'system'
         catalog = self.context.portal_catalog
-        profile = catalog({"Creator":ownerId, "Type":"Profile"})[0]
-        return profile.blogTheme
+        profile = catalog({"Creator":ownerId, "Type":"Profile"})
+        if len(profile) == 0:
+            return u'default'
+        return profile[0].blogTheme
 
 
 class GetContentType(BrowserView):
